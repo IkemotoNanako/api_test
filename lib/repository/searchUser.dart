ff54a91bbeff831cf51179990afe37260cc89129
IkemotoNanako <chibencaizi@gmail.com>
2023-02-01 21:23:13 +0900

    エラーと検索中のページ作成

--- lib/repository/searchUser.dart
+++ lib/repository/searchUser.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:api_test/importer.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -6,24 +7,31 @@ import '../provider/provider.dart';
 
 final searchUsersProvider = FutureProvider<List<GithubUser>>((ref) async {
   final flag = ref.watch(organizationFlagProvider);
-  var response;
-  if (flag == true) {
-    response = await http.get(Uri.https('api.github.com', '/search/users', {
-      'q': ref.watch(searchWordProvider) + "type:org",
-      'sort': 'followers',
-    }));
-  } else {
-    response = await http.get(Uri.https('api.github.com', '/search/users',
-        {'q': ref.watch(searchWordProvider), 'sort': 'followers'}));
-  }
-  if (response.statusCode == 200) {
-    List<GithubUser> list = [];
-    Map<String, dynamic> decoded = json.decode(response.body);
-    for (var item in decoded['items']) {
-      list.add(GithubUser.fromJson(item));
+  try {
+    var response;
+    if (flag == true) {
+      response = await http.get(Uri.https('api.github.com', '/search/users', {
+        'q': ref.watch(searchWordProvider) + "type:org",
+        'sort': 'followers',
+      }));
+    } else {
+      response = await http.get(Uri.https('api.github.com', '/search/users',
+          {'q': ref.watch(searchWordProvider), 'sort': 'followers'}));
     }
-    return list;
-  } else {
-    throw Exception('Fail to search repository');
+    switch (response.statusCode) {
+      case 200:
+        List<GithubUser> list = [];
+        Map<String, dynamic> decoded = json.decode(response.body);
+        for (var item in decoded['items']) {
+          list.add(GithubUser.fromJson(item));
+        }
+        return list;
+      default:
+        ref.watch(codeProvider.notifier).update((state) => state = 1);
+        return [];
+    }
+  } on SocketException catch (_) {
+    ref.watch(codeProvider.notifier).update((state) => state = 2);
+    return [];
   }
 });
