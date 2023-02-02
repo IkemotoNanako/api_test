8327d4c171f250f1e137a9881e6c67d1c379ee43
Kenta Wakasa <wakasa.kenta.pg@gmail.com>
2023-02-02 22:01:19 +0900

    codeProviderの命名について

--- lib/provider/provider.dart
+++ lib/provider/provider.dart
@@ -4,4 +4,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final StateProvider<List<GithubUser>> userProvider = StateProvider((ref) => []);
 final organizationFlagProvider = StateProvider((ref) => false);
 final searchWordProvider = StateProvider((ref) => '');
+
+/// 何を提供するProviderなのか、変数名だけで明確になっていたほうがよい。
 final codeProvider = StateProvider((ref) => 0);
