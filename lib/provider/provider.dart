01dade94f83adc51ed1f85ce99a525d54468465b
Kenta Wakasa <wakasa.kenta.pg@gmail.com>
2023-02-02 22:21:01 +0900

    命名について

--- lib/provider/provider.dart
+++ lib/provider/provider.dart
@@ -2,6 +2,10 @@ import 'package:api_test/core/githubUser.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final StateProvider<List<GithubUser>> userProvider = StateProvider((ref) => []);
+
+/// showOnlyOrganizationProvider とかなら直感的だろうか
+/// 命名規則についてはこちらが参考になる
+///https://qiita.com/YutaManaka/items/62dda256bb7ba6c08399
 final organizationFlagProvider = StateProvider((ref) => false);
 final searchWordProvider = StateProvider((ref) => '');
 
